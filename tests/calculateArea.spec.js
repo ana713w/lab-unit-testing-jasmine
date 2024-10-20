0a46ee25bdca3f989d2cf138c884d75aac037d68
Ana Molina <ana713w@gmail.com>
2024-10-20 10:39:09 +0200

    done

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,27 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
 
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
+
+        it("should take two arguments", () => {
+            expect(calculateArea.length).toBe(2);
+        });
+
+        it("should return a number representing the area", () => {
+            expect(calculateArea(2, 2)).toEqual(4);
+        });
+
+        it("should return undefined if any the arguments is not provided", () => {
+            expect(calculateArea(1)).toEqual(undefined);
+            expect(calculateArea()).toEqual(undefined);
+            expect(calculateArea(undefined, 2)).toEqual(undefined);
+        });
 
     })    
 })
-
-*/
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,28 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
+
+        it("should take two arguments", () => {
+            expect(divide.length).toBe(2);
+        });
+
+        it("should return the division of the two numbers", () => {
+            expect(divide(100, 50)).toEqual(2);
+            expect(divide(1, 2)).toEqual(0.5);
+        });
+
+        it("should return undefined it any of the arguments is not provided", () => {
+            expect(divide()).toEqual(undefined);
+            expect(divide(1)).toEqual(undefined);
+            expect(divide(undefined, 2)).toEqual(undefined);
+        });
 
     })    
 })
 
-*/
